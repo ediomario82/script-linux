a8a78d4138c4089b1aed51d1ef61998932b772d9
ediomario82 <144544973+ediomario82@users.noreply.github.com>
2024-03-14 13:55:27 -0300

    Update incremental_mysql.sh

--- incremental_mysql.sh
+++ incremental_mysql.sh
@@ -3,7 +3,8 @@
 # Configurações do banco de dados
 DB_USER="root"
 DB_PASSWORD="passwd"
-DB_NAME="meubanco"
+DB_NAME="meuDB"
+QTD_BACKUP=3
 
 # Diretório de backup
 BACKUP_DIR="/backupdb/"
@@ -15,14 +16,14 @@ LAST_BACKUP=$(ls -t "$BACKUP_DIR" | head -n 1)
 # Se não houver backups anteriores, faça um backup completo
 if [ -z "$LAST_BACKUP" ]; then
     BACKUP_FILE="$BACKUP_DIR/$TIMESTAMP-full.sql.gz"
-    mysqldump -u"$DB_USER" -p"$DB_PASSWORD" --single-transaction "$DB_NAME" > "$BACKUP_FILE"
+    mysqldump -u"$DB_USER" -p"$DB_PASSWORD" --single-transaction "$DB_NAME" | pv | gzip > "$BACKUP_FILE"
     echo "Backup completo realizado em $TIMESTAMP: $BACKUP_FILE"
     exit 0
 fi
 
 # Backup incremental
 BACKUP_FILE="$BACKUP_DIR/$TIMESTAMP-incremental.sql.gz"
-mysqldump -u"$DB_USER" -p"$DB_PASSWORD" --single-transaction --databases "$DB_NAME" --no-create-info --skip-triggers --skip>
+mysqldump -u"$DB_USER" -p"$DB_PASSWORD" --single-transaction --databases "$DB_NAME" --no-create-info --skip-triggers --skip-comments --compact | pv | gzip > "$BACKUP_FILE"
 
 # Comparar com o último backup e salvar as alterações
 CHANGES_FILE="$BACKUP_DIR/$TIMESTAMP-changes.sql"
@@ -37,3 +38,6 @@ else
     echo "Nenhuma alteração desde o último backup."
     rm "$BACKUP_FILE"  # Não há alterações, remova o arquivo incremental
 fi
+
+# Excluir backups antigos, mantendo apenas os últimos 2
+#ls -t | grep "$BACKUP_FILE" | tail -n +$(($QTD_BACKUP + 1)) | xargs -d '\n' rm -f
